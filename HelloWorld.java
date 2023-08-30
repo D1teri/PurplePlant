e82c996d82f6c9dcac8044b2abdc998bba136616
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2023-08-30 11:24:24 -0700

    Improving code

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World!");
+        System.out.println("Error, please delete yourself, you are not a programmer!!!");
     }
 }
